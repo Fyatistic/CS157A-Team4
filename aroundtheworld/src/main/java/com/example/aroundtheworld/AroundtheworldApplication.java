d5855b0ea45cd338e6d92e5c3c1c62f36961d6a0
victorm648 <89431710+victorm648@users.noreply.github.com>
2021-11-13 22:59:32 -0800

    Add files via upload

---
+++ aroundtheworld/src/main/java/com/example/aroundtheworld/AroundtheworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.aroundtheworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AroundtheworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AroundtheworldApplication.class, args);
+	}
+
+}
---
+++ aroundtheworld/src/test/java/com/example/aroundtheworld/AroundtheworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aroundtheworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AroundtheworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
