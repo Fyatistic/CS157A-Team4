ad32cf1cab0b3440dbb881991df9f172127a5851
victorm648 <89431710+victorm648@users.noreply.github.com>
2021-11-27 00:22:05 -0800

    Add files via upload

---
+++ aroundtheworld/src/main/java/com/example/aroundtheworld/UserService.java
@@ -0,0 +1,7 @@
+package com.example.aroundtheworld;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService{
+	User save(UserRegistrationDto registrationDto);
+}
