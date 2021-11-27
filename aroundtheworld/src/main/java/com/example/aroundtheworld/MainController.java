eec6e275b1deae06f7cf3d3f4515588076c93503
victorm648 <89431710+victorm648@users.noreply.github.com>
2021-11-27 03:28:54 -0800

    Add files via upload

---
+++ aroundtheworld/src/main/java/com/example/aroundtheworld/MainController.java
@@ -0,0 +1,23 @@
+package com.example.aroundtheworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/")
+	public String home() {
+		return "index";
+	}
+
+    @GetMapping("/error")
+    public String error(){
+        return "error!";
+    }
+}
